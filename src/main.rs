8841412a6dc97d7449bd7c6562e2f89a5a4d344f
Yakkhini <59007159+Yakkhini@users.noreply.github.com>
2022-06-21 00:47:46 +0800

    License: Mulan PSL v2

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
+/*
+Copyright (c) 2022 Yakkhini
+Weye is licensed under Mulan PSL v2.
+You can use this software according to the terms and conditions of the Mulan PSL v2.
+You may obtain a copy of Mulan PSL v2 at:
+         http://license.coscl.org.cn/MulanPSL2
+THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+See the Mulan PSL v2 for more details.
+*/
+
 use gtk::prelude::*;
 use gtk::Application;
 use gtk::ApplicationWindow;
+
+use libappindicator::{AppIndicator, AppIndicatorStatus};
 
 fn main() {
     let app = Application::builder()
         .application_id("yakkhini.weye")
         .build();
+
+    let mut indicator = AppIndicator::new("libappindicator test application", "");
+    indicator.set_status(AppIndicatorStatus::Active);
 
     app.connect_activate(build_ui);
 
