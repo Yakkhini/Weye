6fd8485c312ffec05629417251f696f283a20ea1
Yakkhini <59007159+Yakkhini@users.noreply.github.com>
2022-06-22 20:48:01 +0800

    Feat: Every screenshot way in menu.

--- src/tray.rs
+++ src/tray.rs
@@ -29,39 +29,71 @@ pub fn build_tray() {
 
     let mut menu = gtk::Menu::new();
 
+    let menu_select = gtk::MenuItem::with_label("Screenshot");
+    let menu_window = gtk::MenuItem::with_label("Window shot");
     let menu_fulshot = gtk::MenuItem::with_label("Full-screen shot");
     let menu_exit = gtk::MenuItem::with_label("Exit");
 
-    menu_fulshot.connect_activate(move |_| {
+    menu_select.connect_activate(|_| {
+        let config = config_parse::config_parse();
+
+        let select_path = config_parse::save_path_gen(&config.config_selecte);
+
+        let delay = time::Duration::from_secs(config.config_selecte.delay.try_into().unwrap());
+
+        screenshot_command(&select_path, &delay, "area");
+    });
+
+    menu_window.connect_activate(|_| {
+        let config = config_parse::config_parse();
+
+        let window_path = config_parse::save_path_gen(&config.config_window);
+
+        let delay = time::Duration::from_secs(config.config_window.delay.try_into().unwrap());
+
+        screenshot_command(&window_path, &delay, "window");
+    });
+
+    menu_fulshot.connect_activate(|_| {
         let config = config_parse::config_parse();
 
         let fulshot_path = config_parse::save_path_gen(&config.config_fulshot);
 
         let delay = time::Duration::from_secs(config.config_fulshot.delay.try_into().unwrap());
 
-        let notify = fulshot_path.clone() + "saved";
-        thread::sleep(delay);
-
-        Command::new("grimshot")
-            .arg("save")
-            .arg("output")
-            .arg(fulshot_path)
-            .output()
-            .expect("grimshot excute failed");
-        Command::new("notify-send")
-            .arg("--app-name=Weye")
-            .arg("--expire-time=3000")
-            .arg(notify)
-            .output()
-            .expect("notify excute failed");
+        screenshot_command(&fulshot_path, &delay, "output");
     });
+
     menu_exit.connect_activate(|_| {
         gtk::main_quit();
     });
 
+    menu.append(&menu_select);
+    menu.append(&menu_window);
     menu.append(&menu_fulshot);
     menu.append(&menu_exit);
     indicator.set_menu(&mut menu);
 
     menu.show_all();
+}
+
+fn screenshot_command(path: &String, delay: &time::Duration, shot_type: &str) {
+    let notify = path.clone() + " saved";
+    thread::sleep(*delay);
+
+    Command::new("grimshot")
+        .arg("save")
+        .arg(shot_type)
+        .arg(path)
+        .output()
+        .expect("grimshot excute failed");
+
+    thread::sleep(time::Duration::from_secs(2));
+
+    Command::new("notify-send")
+        .arg("--app-name=Weye")
+        .arg("--expire-time=3000")
+        .arg(notify)
+        .output()
+        .expect("notify excute failed");
 }
